71a13c4fef89f7c8a24dc5fcba83fe661b46ed5e
papiro <pierre.pirault@outlook.com>
2016-03-30 21:31:44 -0400

    Simplified the router to now work just based off of the hash and a markupDir parameter. - if the html file will be called something different than the hash, an   optional routes parameter can be passed in to define that mapping.

--- src/router.js
+++ src/router.js
@@ -4,12 +4,16 @@ livity.router = (function() {
 var dom = livity.dom
 ,   ajax = livity.ajax
 
-var router = function (view) {
+var router = function (config) {
+  var routes = config.routes || {}
   dom().listen('hashchange', function() {
     var hash = window.location.hash
-    ajax.GET(dom('#'+hash.slice(1)).at('src'), function (req) {
-      dom(view).inner(req.response)
-      dom(document).trigger('view.'+hash.slice(2))
+    ,   route = config.routes.hasOwnProperty(hash) ? config.routes[hash] : hash
+    // slice off the leading '#' and a following '/' if there is one
+    route = route.slice(route[1]==='/'?2:1)
+    ajax.GET(config.markupDir+'/'+route+'.html', function (req) {
+      dom(config.view).inner(req.response)
+      dom(document).trigger('view.'+route)
     })
     router.initialized = true
   })
